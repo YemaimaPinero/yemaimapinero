bb70d8bb83fd89cfbb3aa25c699efda83911997f
YemaimaPinero <98278494+YemaimaPinero@users.noreply.github.com>
2023-03-01 22:25:19 -0300

    Proyecto Final Integrador
    Creación de recursos

---
+++ CV 2023/app.js
@@ -0,0 +1,25 @@
+//MENU LATERAL
+var menu_visible=false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu() {
+    if (menu_visible==false) {
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+//OCULTAR MENU LATERAL UNA EZ SELECCIONADA LA OPCION
+
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x < links.length; x++){
+    links[x].onClick= function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+
